1ca8d18d036fb67049f6a7229649be36b5608eb0
raydanielg <airezra2+1@gmail.com>
2026-07-30 15:02:53 +0300

    commit

--- front/components/feature-grid.tsx
+++ front/components/feature-grid.tsx
@@ -117,7 +117,7 @@ function FeatureCard({
 
 export function FeatureGrid() {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="grid grid-cols-2 gap-3 sm:gap-4">
       {features.map((feature, i) => (
         <FeatureCard key={feature.title} feature={feature} index={i} />
       ))}
