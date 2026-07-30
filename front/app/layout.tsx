b2387b50ae657ddae39879b3c1f336dd855de639
raydanielg <airezra2+1@gmail.com>
2026-07-30 15:17:48 +0300

    commit

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -47,6 +48,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <link rel="icon" href="/food-irradiation.png" />
         <TooltipProvider>{children}</TooltipProvider>
+        <Analytics />
       </body>
     </html>
   );
